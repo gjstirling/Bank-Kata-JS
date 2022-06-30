2b521568cbf9ba0412e882ba2ed3ee80b57a2bc7
Graeme Stirling <graemejstirling@gmail.com>
2022-06-30 16:56:49 +0100

    Can now print a single transaction

--- spec/unit/StatementSpec.js
+++ spec/unit/StatementSpec.js
@@ -5,5 +5,12 @@ describe("Statement", () => {
         const assertion = statement.print([])
         expect(assertion).toEqual("date || credit || debit || balance\n")
     });
+
+    it("prints a single transaction", () => {
+        statement = new Statement 
+        const assertion = statement.print([{credit: 100, date: "28/06/2022"}])
+        expect(assertion).toEqual("date || credit || debit || balance\n28/06/2022 || 100.00 || || 100.00\n")
+    });
   });
 });
+
--- src/Statement.js
+++ src/Statement.js
@@ -1,5 +1,28 @@
 class Statement {
-    print() {
-       return "date || credit || debit || balance\n" 
+  print(transactions) {
+    let balance = 0;
+    let transaction = "";
+    let transactionStrings = [];
+    for (let i = 0; i < transactions.length; i++) {
+      if (transactions[i].credit) {
+        transaction = transactions[i].credit;
+        balance += transaction;
+        transactionStrings.push(
+          `${transactions[i].date} || ${transaction.toFixed(
+            2
+          )} || || ${balance.toFixed(2)}\n`
+        );
+      } else {
+        transaction = transactions[i].debit;
+        balance -= transaction;
+        transactionStrings.push(
+          `${transactions[i].date} || || ${transaction.toFixed(
+            2
+          )} || ${balance.toFixed(2)}\n`
+        );
+      }
     }
+    transactionStrings.push("date || credit || debit || balance\n");
+    return transactionStrings.reverse().join("");
+  }
 }
