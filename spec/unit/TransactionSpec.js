61cc36956083cd90a7e012f25dd36cb4d0e8e887
Graeme Stirling <graemejstirling@gmail.com>
2022-06-30 12:23:19 +0100

    Mocking date for deposit

--- spec/unit/TransactionSpec.js
+++ spec/unit/TransactionSpec.js
@@ -1,23 +1,27 @@
+describe("Transaction", () => {
+  beforeEach(() => {
+    transaction = new Transaction();
+    jasmine.clock().install();
+  });
 
-describe('Transaction', () => {
+  it("Creates a deposit", () => {
+    var baseTime = new Date(2022, 5, 28);
+    jasmine.clock().mockDate(baseTime);
+    const assertion = transaction.addDeposit(100.0);
+    expect(assertion).toEqual({ credit: 100.0, date: "28/06/2022" });
+  });
 
-    beforeEach(() => {
-        transaction = new Transaction
-    })
+  it("requires an amount to create transaction", () => {
+    const assertion = transaction.addDeposit();
+    expect(assertion).toEqual({ error: "argument invalid" });
+  });
 
-    it('Creates a deposit', ()=>{
-        const assertion = transaction.addDeposit(100.00)
-        expect(assertion).toEqual({credit: 100.00, date: "date"});
-    });
+  it("requires an number to create transaction", () => {
+    const assertion = transaction.addDeposit("not a number");
+    expect(assertion).toEqual({ error: "argument invalid" });
+  });
 
-    it('requires an amount to create transaction', () => {
-        const assertion = transaction.addDeposit()
-        expect(assertion).toEqual({error: "argument invalid"});
-    })
-
-    it('requires an number to create transaction', () => {
-        const assertion = transaction.addDeposit("not a number")
-        expect(assertion).toEqual({error: "argument invalid"});
-    })
-
+  afterEach(function () {
+    jasmine.clock().uninstall();
+  });
 });
--- src/Transaction.js
+++ src/Transaction.js
@@ -1,10 +1,21 @@
 class Transaction {
-
-    addDeposit(amount) {
-        if (amount === undefined || (typeof(amount) !== 'number')) {
-            return {error: "argument invalid"};
-        }
-        const transaction = {credit: amount, date: "date"};
-        return transaction;
+  addDeposit(amount) {
+    if (this.#argumentError(amount)) {
+      return { error: "argument invalid" };
     }
+    let currentDate = new Date();
+    currentDate = this.#formatDate(currentDate);
+    return { credit: amount, date: currentDate };
+  }
+
+  #argumentError(value) {
+    return value === undefined || typeof value !== "number";
+  }
+
+  #formatDate(date) {
+    let day = date.getDate().toString().padStart(2, "0");
+    let month = (date.getMonth() + 1).toString().padStart(2, "0");;
+    let year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 }
