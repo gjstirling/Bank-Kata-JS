b41c2fb87b1a2dd7d0630592a913e66135819d8a
Graeme Stirling <graemejstirling@gmail.com>
2022-07-01 11:18:20 +0100

    Refactoring reused logic

--- src/Transaction.js
+++ src/Transaction.js
@@ -7,20 +7,14 @@ class Transaction {
     if (this.#argumentError(amount)) {
       return { error: "argument invalid" };
     }
-    const currentDate = this.#formatDate(new Date());
-    const transaction = { credit: amount, date: currentDate };
-    this.transactionHistory.push(transaction);
-    return transaction;
+    return this.#createTransaction(amount, "deposit");
   }
 
   addWithdrawel(amount) {
     if (this.#checkFunds(amount)) {
-      return {error: "Account credit too low, transaction cancelled"}
+      return { error: "Account credit too low, transaction cancelled" };
     }
-    const currentDate = this.#formatDate(new Date());
-    const transaction = { debit: amount, date: currentDate };
-    this.transactionHistory.push(transaction);
-    return transaction;
+    return this.#createTransaction(amount, "withdrawal");
   }
 
   getTransactions() {
@@ -28,16 +22,15 @@ class Transaction {
   }
 
   #checkFunds(amount) {
-    let balance = 0
-    for (let i = 0; i< this.transactionHistory.length; i++) {
+    let balance = 0;
+    for (let i = 0; i < this.transactionHistory.length; i++) {
       if (this.transactionHistory[i].credit) {
-        balance += this.transactionHistory[i].credit*100
-      }
-      else {
-        balance -= this.transactionHistory[i].debit*100
+        balance += this.transactionHistory[i].credit * 100;
+      } else {
+        balance -= this.transactionHistory[i].debit * 100;
       }
     }
-    balance -= amount*100
+    balance -= amount * 100;
     return balance < 0;
   }
 
@@ -50,5 +43,17 @@ class Transaction {
     let month = (date.getMonth() + 1).toString().padStart(2, "0");
     let year = date.getFullYear();
     return `${day}/${month}/${year}`;
+  }
+
+  #createTransaction(amount, type) {
+    const currentDate = this.#formatDate(new Date());
+    let transaction;
+    if (type === "deposit") {
+      transaction = { credit: amount, date: currentDate };
+    } else {
+      transaction = { debit: amount, date: currentDate };
+    }
+    this.transactionHistory.push(transaction);
+    return transaction;
   }
 }
